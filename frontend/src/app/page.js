7cfa0900a177d9bfacf3e3252e498757eb3664c9
Iamharshnavare <harshnavare11@gmail.com>
2026-01-18 16:52:17 +0530

    updation of product page and design of homepage

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,11 +2,12 @@ import Login from "@/app/sign-in/login/page"
 import CheckoutPage from "./cartsystem/checkout/page";
 import CraftedRootsProduct from "./product/page";
 import CraftedRootsHomepage from "@/app/shoppage/page";
+import ShopPage from "@/app/shoppage/page";
 export default function Home() {
   return (
    <>
    <div className="bg-[#FFF9EF] h-screen w-screen">
-    <CraftedRootsHomepage/>
+    <ShopPage/>
    </div>
    </>
   );
