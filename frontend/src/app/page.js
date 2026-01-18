d91d427a5bf3470c223eef79287e3951fe73bf0c
Iamharshnavare <harshnavare11@gmail.com>
2026-01-18 15:59:22 +0530

    Upadte Product detail page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,12 +1,12 @@
 import Login from "@/app/sign-in/login/page"
 import CheckoutPage from "./cartsystem/checkout/page";
 import Shop from "@/app/shoppage/page"
-
+import CraftedRootsProduct from "./product/page";
 export default function Home() {
   return (
    <>
    <div className="bg-[#FFF9EF] h-screen w-screen">
-    <Shop/>
+    <CraftedRootsProduct/>
    </div>
    </>
   );
