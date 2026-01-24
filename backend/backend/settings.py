c3e9864701747502486eb151fcd2986f8be4e9ae
Akshatchourey <achourey09@gmail.com>
2026-01-24 12:25:30 +0530

    boto3 AWS storage bugfix.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework.authtoken',
     'corsheaders',
+    'storages',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
 ]
